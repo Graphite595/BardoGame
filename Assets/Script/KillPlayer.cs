2e75b542e1424dd4d0654d0014c48297a4349c31
DESKTOP-H6I6RED\Gerson <graphite595@gmail.com>
2021-01-31 13:29:11 -0500

    Added Enemies with instakill

---
+++ Assets/Script/KillPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlayer : MonoBehaviour
+{
+    public GameObject spawnpoint;
+    public AudioSource muerte;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.name == "Jugador")
+        {
+            collision.transform.position = spawnpoint.transform.position;
+            muerte.Play();
+        }
+    }
+}
---
+++ Assets/Script/MonsterControllerScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterControllerScript : MonoBehaviour
+{
+    public Animator anim;
+    public Rigidbody2D mostro;
+    public float movespeed;
+    private float movingtime;
+    public float refmovingtime;
+    private float waittime;
+    public float refwaittime;
+    private bool moving=true;
+    private bool facingright = true;
+    void Start()
+    {
+        movingtime = refmovingtime;
+        waittime = refwaittime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moving)
+        {
+            if (facingright)
+            {
+                movingtime -= Time.deltaTime;
+                mostro.velocity = new Vector2(movespeed, mostro.velocity.y);
+                anim.SetFloat("MovX", movespeed);
+            }
+            else
+            {
+                movingtime -= Time.deltaTime;
+                mostro.velocity = new Vector2(movespeed*(-1), mostro.velocity.y);
+                anim.SetFloat("MovX", movespeed*(-1));
+            }
+        }
+        else
+        {
+            waittime -= Time.deltaTime;
+            mostro.velocity = new Vector2(0f, mostro.velocity.y);
+            anim.SetFloat("MovX", 0f);
+        }
+
+        if (movingtime <= 0f)
+        {
+            moving = false;
+            facingright = !facingright;
+            movingtime = refmovingtime;
+        }
+        if (waittime <= 0f)
+        {
+            moving = true;
+            waittime = refwaittime;
+        }
+        if (mostro.velocity.x != 0f)
+        {
+            anim.SetFloat("LastMovX",mostro.velocity.x);
+        }
+    }
+}
